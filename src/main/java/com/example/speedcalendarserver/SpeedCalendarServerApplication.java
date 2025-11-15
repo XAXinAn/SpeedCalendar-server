054d7f7ed2758fd1d204bff137a2a22765f14543
Jiang Hangxi <jianghangximail@gmail.com>
2025-11-15 11:17:42 +0800

    初始文件

---
+++ src/main/java/com/example/speedcalendarserver/SpeedCalendarServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.speedcalendarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedCalendarServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedCalendarServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/speedcalendarserver/SpeedCalendarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speedcalendarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedCalendarServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
